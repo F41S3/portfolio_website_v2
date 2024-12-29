25c65ec4c38845cc252cb4ff93351a008a33400e
Alex Pettipas <67800428+F41S3@users.noreply.github.com>
2024-12-29 01:28:16 -0400

    Implemented image slider and video container elements. Added some content to display

--- src/element/SliderContainer.js
+++ src/element/SliderContainer.js
@@ -4,7 +4,6 @@ import './css/styles.css'
 
 export default function SliderContainer({image}) {
 
-
     return (
         <div className="imageSlider">
             <img src={image} alt="Placeholder"/>

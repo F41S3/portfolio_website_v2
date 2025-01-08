8e3afe839a6eb859db9364557df274e6ae92cada
Alex Pettipas <67800428+F41S3@users.noreply.github.com>
2025-01-08 03:47:20 -0400

    Added secret image page

---
+++ src/pages/Secret.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <img className="selfie" src="../../public/media/art.png" alt="Secret"></img>
+        </div>
+
+    );
+}
